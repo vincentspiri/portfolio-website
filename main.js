f732a61952265581651d0e1f0912e65073f15374
Vincent Spiri <vincentspiri@yahoo.com>
2023-01-23 17:43:03 -0600

    Initial

---
+++ main.js
@@ -0,0 +1,11 @@
+document.querySelector(".cards").onmousemove = e => {
+    for (const card of document.querySelectorAll(".card")) {
+        const rect = card.getBoundingClientRect(),
+            x = e.clientX - rect.left;
+        y = e.clientY - rect.top;
+
+        card.style.setProperty("--mouse-x", `${x}px`);
+        card.style.setProperty("--mouse-y", `${y}px`)
+
+    }
+}
