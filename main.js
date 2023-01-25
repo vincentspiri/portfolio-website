07e9e9562fd4da2b3f33cd67bb6d0e54c5a96742
Vincent Spiri <vincentspiri@yahoo.com>
2023-01-25 05:44:05 -0600

    Made website responsive with Bootstrap

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
-document.querySelector(".cards").onmousemove = e => {
-    for (const card of document.querySelectorAll(".card")) {
+document.querySelector(".lmaos").onmousemove = e => {
+    for(const card of document.querySelectorAll(".lmao")) {
         const rect = card.getBoundingClientRect(),
-            x = e.clientX - rect.left;
+        x = e.clientX - rect.left;
         y = e.clientY - rect.top;
 
-        card.style.setProperty("--mouse-x", `${x}px`);
-        card.style.setProperty("--mouse-y", `${y}px`)
-
+        card.style.setProperty("--test-x", `${x}px`);
+        card.style.setProperty("--test-y", `${y}px`);
     }
 }
+
+const hiddenElements = document.querySelectorAll(".hidden");
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(e => {
+        if(e.isIntersecting) {
+            e.target.classList.add("show");
+            observer.unobserve(e);
+        }
+    });
+}, {threshold: 1});
+hiddenElements.forEach(e => observer.observe(e));
